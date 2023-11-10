9e40be140505c49e352bab0179c3e98c6e5f919f
German Antonio <german@tiralinea.com>
2023-11-10 05:00:04 -0300

    added Vec2 vector class with overloaded operators

--- SFML Game/Vec2.h
+++ SFML Game/Vec2.h
@@ -1,6 +1,7 @@
 #pragma once
 
-class Vec2 {
+class Vec2
+{
 public:
   float x = 0;
   float y = 0;
@@ -8,19 +9,19 @@ public:
   Vec2();
   Vec2(float xin, float yin);
 
-  bool operator == (const Vec2 &rhs) const;
-  bool operator != (const Vec2 &rhs) const;
+  bool operator==(const Vec2& rhs) const;
+  bool operator!=(const Vec2& rhs) const;
 
-  Vec2 operator + (const Vec2 &rhs) const;
-  Vec2 operator - (const Vec2 &rhs) const;
-  Vec2 operator / (const float val) const;
-  Vec2 operator * (const float val) const;
+  Vec2 operator+(const Vec2& rhs) const;
+  Vec2 operator-(const Vec2& rhs) const;
+  Vec2 operator/(const float val) const;
+  Vec2 operator*(const float val) const;
 
-  void operator += (const Vec2& rhs);
-  void operator -= (const Vec2& rhs);
-  void operator *= (const float val);
-  void operator /= (const float val);
+  void operator+=(const Vec2& rhs);
+  void operator-=(const Vec2& rhs);
+  void operator*=(const float val);
+  void operator/=(const float val);
 
-  float dist(const Vec2 &rhs) const;
+  float dist(const Vec2& rhs) const;
   float length() const;
 };
