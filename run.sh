a4ea6eca5a73074f514d269de225960903a0e2ed
German Antonio <german@tiralinea.com>
2023-11-11 11:54:04 -0300

    fixed linker errors

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -o binary-executable main.cpp -lsfml-graphics -lsfml-window -lsfml-system
+g++ -std=c++17 -o output ./src/main.cpp ./src/Entity.cpp ./src/EntityManager.cpp ./src/Game.cpp ./src/Vec2.cpp -lsfml-graphics -lsfml-window -lsfml-system
 ./output
