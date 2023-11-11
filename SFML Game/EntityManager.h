e130cf40048b30b17f729486b98c6fd578973b0b
German Antonio <german@tiralinea.com>
2023-11-11 02:59:49 -0300

    WIP remove dead entities

--- SFML Game/EntityManager.h
+++ SFML Game/EntityManager.h
@@ -1,19 +1,21 @@
 #pragma once
 
-#include <memory>
-#include <string>
+#include "Entity.h"
 
-#include "Components.h"
+#include <map>
+#include <vector>
 
-typedef std::vector<std::shared_ptr<Entity>> EntityVec; // 'EntityVec' as an alias of all that's before it after typedef
+typedef std::vector<std::shared_ptr<Entity>> EntityVec; // EntityVec is an alias for all that's before it after typedef
 typedef std::map<std::string, EntityVec> EntityMap;     // Same for EntityMap
 
 class EntityManager
 {
   EntityVec m_entities;
-  EntityVec m_toAdd;
+  EntityVec m_entitiesToAdd;
   EntityMap m_entityMap;
   size_t m_totalEntities = 0;
+
+  void removeDeadEntities(EntityVec& vec);
 
 public:
   EntityManager();
@@ -21,5 +23,4 @@ public:
   std::shared_ptr<Entity> addEntity(const std::string& tag);
   EntityVec& getEntities();
   EntityVec& getEntities(const std::string& tag);
-  std::shared_ptr<Entity> addEntity(const std::string& tag);
 };
