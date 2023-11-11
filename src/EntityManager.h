0b059d083bd79b1b889f82ebe1d00a2c38c0857d
German Antonio <german@tiralinea.com>
2023-11-11 11:54:45 -0300

    moved stuff into src and made code functional

---
+++ src/EntityManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Entity.h"
+
+#include <map>
+#include <vector>
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec; // EntityVec is an alias for all that's before it after typedef
+typedef std::map<std::string, EntityVec> EntityMap;     // Same for EntityMap
+
+class EntityManager
+{
+  EntityVec m_entities;
+  EntityVec m_entitiesToAdd;
+  EntityMap m_entityMap;
+  size_t m_totalEntities = 0;
+
+  void removeDeadEntities(EntityVec& vec);
+
+public:
+  EntityManager();
+  void update();
+  std::shared_ptr<Entity> addEntity(const std::string& tag);
+  const EntityVec& getEntities();
+  const EntityVec& getEntities(const std::string& tag);
+};
