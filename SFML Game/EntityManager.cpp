b0e0cbd34449974daea113170c3fed2a7acb6e97
German Antonio <german@tiralinea.com>
2023-11-11 04:13:40 -0300

    added removeDeadEntities functionality

--- SFML Game/EntityManager.cpp
+++ SFML Game/EntityManager.cpp
@@ -13,14 +13,11 @@ void EntityManager::update()
     m_entityMap[e->tag()].push_back(e);
   }
 
-  // remove all of the entities from this vector if their m_active is false.
-  m_entities.erase(std::remove_if(m_entities.begin(), m_entities.end(), [](const auto& e) { return !e.m_active; }),
-                   m_entities.end());
+  removeDeadEntities(m_entities);
 
-  // same for the tagged map
-  for (auto it = m_entityMap.begin(); it != m_entityMap.end();)
+  for (auto& [tag, entityVec] : m_entityMap)
   {
-    it->second.erase(std::remove_if(it->second.begin(), it->second.end(), [](const auto& e) { return !e->m_active; }));
+    removeDeadEntities(entityVec);
   }
 
   m_entitiesToAdd.clear();
@@ -28,9 +25,15 @@ void EntityManager::update()
 
 std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
 {
-  auto e = std::shared_ptr<Entity>(
+  auto entity = std::shared_ptr<Entity>(
       new Entity(m_totalEntities++, tag)); // this syntax is for private constructors (instead of make_shared)
 
-  m_entitiesToAdd.push_back(e); // push the shared pointer of newly instanced entity e to the toAdd vector
-  return e;             // we are preventing iteration invalidation (i.e. modifying iterators during their iteration)
+  m_entitiesToAdd.push_back(entity); // push the shared pointer of newly instanced entity e to the toAdd vector
+  return entity; // we are preventing iteration invalidation (i.e. modifying iterators during their iteration)
+}
+
+// remove all of the entities from this vector if their m_active is false.
+void EntityManager::removeDeadEntities(EntityVec& vec)
+{
+  vec.erase(std::remove_if(vec.begin(), vec.end(), [](const auto& e) { return !e.m_active; }), vec.end());
 }
