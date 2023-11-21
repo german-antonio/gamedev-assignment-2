025e76fe731f66db279e6c0b4dfc6862ad44035c
German Antonio <german@tiralinea.com>
2023-11-21 00:32:27 -0300

    added debug run shell script

---
+++ debug-run.sh
@@ -0,0 +1,2 @@
+g++ -std=c++17 -g -o output ./src/main.cpp ./src/Entity.cpp ./src/EntityManager.cpp ./src/Game.cpp ./src/Vec2.cpp ./src/Utils.cpp -lsfml-graphics -lsfml-window -lsfml-system
+gdb ./output
