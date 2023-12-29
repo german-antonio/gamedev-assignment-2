c426c0b108401f6b3d5aae6ec5b4d861972e107a
German Antonio <german@tiralinea.com>
2023-12-29 03:22:04 -0300

    added toUint8 utils function for converting to this type

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3,3 +3,4 @@
 #include "Utils.h"
 
 const int Utils::randBetween(int min, int max) { return (rand() % (max - min)) + min; }
+const sf::Uint8 Utils::toUint8(int number) { return static_cast<sf::Uint8>(number); }
--- src/Utils.h
+++ src/Utils.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 namespace Utils
 {
 const int randBetween(int min, int max);
+const sf::Uint8 toUint8(int number);
 };
