616f3633cc215a26516982a0f1c4ed041d7c59ef
German Antonio <german@tiralinea.com>
2024-02-08 02:56:03 -0300

    improved CTransform for acceleration and added CEdge

--- src/Components.h
+++ src/Components.h
@@ -1,16 +1,36 @@
 #pragma once
 
 #include "Vec2.h"
+#include "cmath"
 #include <SFML/Graphics.hpp>
 
 class CTransform
 {
 public:
+  // Position
   Vec2 pos = {0.0, 0.0};
+  Vec2 maxPos = {0.0, 0.0};
+  Vec2 minPos = {0.0, 0.0};
+  // Velocity
   Vec2 velocity = {0.0, 0.0};
+  Vec2 maxVel = {0.0, 0.0};
+  Vec2 minVel = {0.0, 0.0};
+  // Acceleration
+  Vec2 acceleration = {0.0, 0.0};
+  float accelerationStep = 0.0f;
+  // Others
   float angle = 0;
 
   CTransform(const Vec2& p, const Vec2& v, float a) : pos(p), velocity(v), angle(a) {}
+  CTransform(const Vec2& p, const Vec2& maxp, const Vec2& minp, const Vec2& v, const Vec2& maxv, const Vec2& minv,
+             const Vec2& acc, float step, float a)
+      : pos(p), maxPos(maxp), minPos(minp), velocity(v), maxVel(maxv), minVel(minv), acceleration(acc),
+        accelerationStep(step), angle(a)
+  {
+  }
+
+  float getVelScalar() { return std::hypot(velocity.x, velocity.y); }
+  float getAccScalar() { return std::hypot(acceleration.x, acceleration.y); }
 };
 
 class CShape
@@ -25,6 +45,12 @@ public:
     circle.setOutlineThickness(thickness);
     circle.setOrigin(radius, radius);
   }
+};
+class CEdge
+{
+public:
+  int factor = 0;
+  CEdge(int f) : factor(f) {}
 };
 class CCollision
 {
